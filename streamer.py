593e704e8e3b57fa88b3c8cd99c3cadbfed5a8c2
Art <art@the.domain.name>
2021-07-14 01:16:58 +0300

    Cleanup and seperation of model specific tasks into a pipeline function

--- streamer.py
+++ streamer.py
@@ -1,3 +1,4 @@
+import copy
 import os
 import sys
 import json
@@ -11,7 +12,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.exc import NoResultFound
 from sqlalchemy.orm import sessionmaker
 from multiprocessing import Process, Queue
-from models import CompanyProfileStream, CompanyProfileStreamCol, Company, FilingsStream, InsolvencyStream, OfficersStream, ChargesStream, PersonsStream
+from models import CompanyProfileStream, Company, FilingsStream, InsolvencyStream, OfficersStream, ChargesStream, PersonsStream
 from funcs import company_event_process, make_company_event_store
 
 # Base class that contains all the shared methods
@@ -19,31 +20,30 @@ class Streamer:
     api_key = os.environ.get("CH_API_KEY")
     req_session = requests.session()
     req_session.headers = {"User-Agent": "Stream Receiver"}
+    timezone = pytz.timezone("Europe/London")
 
     # DB connection init, class wide
     engine = create_engine('postgresql://postgres:BigBlueBattery123!@localhost:5500/ch')
     Session = sessionmaker(bind=engine)
-    row_session = Session()
-    col_session = Session()
+    session = Session()
 
     # model: SQLAlchemy model class appropriate to the stream type (url)
     # event_function: the function that maps the raw event JSON to model
-    def __init__(self, url, model, col_model, event_function):
+    def __init__(self, url, model, event_function):
         self.model = model
-        self.col_model = col_model
         self.event_to_model = event_function
-        self.timezone = pytz.timezone("Europe/London")
         # Resume from last timepoint found in db
-        last_timepoint = self.row_session.query(model.event_timepoint).order_by(model.event_timepoint.desc()).first()[0]
-        self.url = url + f"?timepoint={last_timepoint+1}"
+        last_timepoint = self.session.query(model.event_timepoint).order_by(model.event_timepoint.desc()).first()[0]
+        self.url = url
         print("Resuming stream from last timepoint of", last_timepoint)
 
         # If this is the companyprofile stream, load up latest event for each company in memory
         # Saves on querying database, which is slow and allows to keep up with realtime stream
         # This event_store is kept updated with each new event replacing old one, or if new company appending a new key
         if self.model == CompanyProfileStream:
-            self.events_store = make_company_event_store(self.row_session)
-        self.stream = Streamer.req_session.get(url, stream=True, auth=requests.auth.HTTPBasicAuth(Streamer.api_key, ""))
+            self.events_store = make_company_event_store(self.session)
+
+        self.stream = Streamer.req_session.get(url + f"?timepoint={last_timepoint+1}", stream=True, auth=requests.auth.HTTPBasicAuth(Streamer.api_key, ""))
 
     # The event and resource keys are consistent across all streams
     # We can initialize the model with those fields populated
@@ -77,113 +77,105 @@ class Streamer:
                        "data_foreign_company_details_accounts_must_file_within_months"]:
                 value = int(value)
 
-            try:
-                if old_event[key] != value:
-                    fields_changed.append(key)
-            except:
-                print(f"------------------------------{type(old_event)} \n", old_event, "\n------------------------------------------")
-                break
+            # try:
+            if old_event[key] != value:
+                fields_changed.append(key)
+            # except:
+            #     print(f"------------------------------{type(old_event)} \n", old_event, "\n------------------------------------------")
+            #     break
 
         return fields_changed
+
 
     # Any new or updated data fires a event, what is done with any of the events is up for debate
     # Currently planned is
     def fire_event(self, event_name, company=None, fields_changed:list=[]):
-        print(event_name, fields_changed)
+        if fields_changed:
+            print(event_name, fields_changed, company.data_company_number)
+        else:
+            print(event_name, company.data_company_number)
+
+    def read_from_stream(self):
+        for line in self.stream.iter_lines():
+            if line:
+                event = json.loads(line.decode('utf-8'))
+                # Extract the base attributes such as event_published_at, resource_id etc
+                model = self.populate_model(event, self.model)
+
+                model = self.pipeline(event, model)
+
+                self.session.add(model)
+                self.session.commit()
+
+    def start_stream(self):
+        # Infinite loop to restart the stream in case of crash (Chunked response)
+        while True:
+            try:
+                self.read_from_stream()
+            except (requests.exceptions.ChunkedEncodingError, requests.exceptions.StreamConsumedError) as e:
+                print("Stream error encountered, restarting stream. ", e)
+                last_timepoint = self.session.query(self.model.event_timepoint)\
+                                .order_by(self.model.event_timepoint.desc()).first()[0]
+                self.stream = Streamer.req_session.get(self.url + f"?timepoint={last_timepoint+1}", stream=True, auth=requests.auth.HTTPBasicAuth(Streamer.api_key, ""))
+
 
 class CompanyStreamer(Streamer):
 
     def company_exists(self, company_number):
-        q = Streamer.row_session.query(Company.CompanyNumber).filter(Company.CompanyNumber == company_number.upper())
-        if Streamer.row_session.query(q.exists()).scalar():
+        q = self.session.query(Company.CompanyNumber).filter(Company.CompanyNumber == company_number.upper())
+        if self.session.query(q.exists()).scalar():
             return True
         else:
             return False
 
-    # Update the snapshot data to be current, and track field changes
-    # Skip all attributes that will not change, such as company number, incorp data, etc
-    def update_company(self, company):
-        pass
+    # All of the custom logic for processing the event individually goes here
+    def pipeline(self, event, model):
+        company = self.event_to_model(event, model)
 
-    def read_from_stream(self):
-        counter = 0
-        for line in self.stream.iter_lines():
-            if line:
-                event = json.loads(line.decode('utf-8'))
-                # Normal companyprofile model instance
-                company = self.populate_model(event, self.model)
-                company = self.event_to_model(event, company)
+        # Check if a event for this company exists in the event_store
+        previous_event = self.events_store.get(company.data_company_number, False)
 
-                # Columnar companyprofile instance model instance
-                # company_col = self.populate_model(event, self.col_model)
-                # company_col = self.event_to_model(event, company_col)
+        self.events_store[company.data_company_number] = copy.deepcopy(company.__dict__)
 
-                # Streamer.col_session.add(company_col)
+        if not previous_event:
+            self.fire_event("companyprofile.new", company=company)
 
-                # Check if a event for this company exists in the event_store
-                previous_event = self.events_store.get(company.data_company_number, False)
+        # If a previous event was found, compare old row with new row and fire off event if any fields are changed
+        if previous_event:
+            changed_fields = self.compare_models(previous_event, company)
+            if changed_fields:
+                self.fire_event("companyprofile.update", fields_changed=changed_fields, company=company)
 
-                self.events_store[company.data_company_number] = company.__dict__
-
-                if not previous_event:
-                    self.fire_event("companyprofile.new", company=company)
-
-                # If a previous event was found, compare old row with new row and fire off event if any fields are changed
-                if previous_event:
-                    changed_fields = self.compare_models(previous_event, company)
-                    if changed_fields:
-                        self.fire_event("companyprofile.update", fields_changed=changed_fields, company=company)
-
-                Streamer.row_session.add(company)
-                Streamer.row_session.commit()
-
-
-
-                counter += 1
-                # if counter % 10000 == 0:
-                #     Streamer.col_session.commit()
-                #     Streamer.row_session.query(CompanyProfileStream).delete()
-                #     Streamer.row_session.commit()
+        return company
 
 
 
 class GenericTempStreamer(Streamer):
 
-    def read_from_stream(self):
-        for line in self.stream.iter_lines():
-            if line:
-                event = json.loads(line.decode('utf-8'))
-                # Normal companyprofile model instance
-                model = self.populate_model(event, self.model)
-                model.company_number = event["resource_uri"].split("/")[2]
+    def pipeline(self, event, model):
+        model.company_number = event["resource_uri"].split("/")[2]
+        model.data = event["data"]
 
-                model.data = event["data"]
-                Streamer.row_session.add(model)
-                Streamer.row_session.commit()
+        return model
 
 if __name__ == "__main__":
     # q = Queue()
     stream_to_launch = sys.argv[1]
-    # Infinite loop here, because the stream can end and needs restarting in those cases
-    while True:
-        if stream_to_launch == "company":
-            # Company profile stream
-            # Resume from last comp in company table timepoint = 30090073
-            streamer = CompanyStreamer("https://stream.companieshouse.gov.uk/companies", CompanyProfileStream, CompanyProfileStreamCol, company_event_process)
+    if stream_to_launch == "company":
+        streamer = CompanyStreamer("https://stream.companieshouse.gov.uk/companies", CompanyProfileStream, company_event_process)
+    # Temporary
+    elif stream_to_launch == "filing":
+        streamer = GenericTempStreamer("https://stream.companieshouse.gov.uk/filings", FilingsStream, company_event_process)
+    elif stream_to_launch == "insolvency":
+        streamer = GenericTempStreamer("https://stream.companieshouse.gov.uk/insolvency-cases", InsolvencyStream, company_event_process)
+    elif stream_to_launch == "officer":
+        streamer = GenericTempStreamer("https://stream.companieshouse.gov.uk/officers", OfficersStream, company_event_process)
+    elif stream_to_launch == "charges":
+        streamer = GenericTempStreamer("https://stream.companieshouse.gov.uk/charges", ChargesStream, company_event_process)
+    elif stream_to_launch == "persons":
+        streamer = GenericTempStreamer("https://stream.companieshouse.gov.uk/persons-with-significant-control", PersonsStream, company_event_process)
 
-        # Temporary
-        elif stream_to_launch == "filing":
-            streamer = GenericTempStreamer("https://stream.companieshouse.gov.uk/filings", FilingsStream, CompanyProfileStreamCol, company_event_process)
-        elif stream_to_launch == "insolvency":
-            streamer = GenericTempStreamer("https://stream.companieshouse.gov.uk/insolvency-cases", InsolvencyStream, CompanyProfileStreamCol, company_event_process)
-        elif stream_to_launch == "officer":
-            streamer = GenericTempStreamer("https://stream.companieshouse.gov.uk/officers", OfficersStream, CompanyProfileStreamCol, company_event_process)
-        elif stream_to_launch == "charges":
-            streamer = GenericTempStreamer("https://stream.companieshouse.gov.uk/charges", ChargesStream, CompanyProfileStreamCol, company_event_process)
-        elif stream_to_launch == "persons":
-            streamer = GenericTempStreamer("https://stream.companieshouse.gov.uk/persons-with-significant-control", PersonsStream, CompanyProfileStreamCol, company_event_process)
-
-        streamer.read_from_stream()
+    streamer.start_stream()
 
 
 
